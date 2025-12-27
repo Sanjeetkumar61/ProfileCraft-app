cc6137e39284fc4d9caf87b91da6382e4c69ee8c
Sanjeet Kumar <156106398+Sanjeetkumar61@users.noreply.github.com>
2025-12-27 15:26:34 +0530

    Update vite.config.js

--- Me-API_Playground/vite.config.js
+++ Me-API_Playground/vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://profilecraft-backend.onrender.com',
         changeOrigin: true,
       },
     },
